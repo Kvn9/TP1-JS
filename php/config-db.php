710f14c0b0db15fab44708533ea0e161fa48a55b
Kvn9 <kevintata02@gmail.com>
2023-10-19 23:35:03 +0200

    First Version

--- php/config-db.php
+++
@@ -1,9 +0,0 @@
-<?php
- // Connection BDD
- $DBDRIVER = 'mysql';
- $DBHOST = 'localhost';
- $DBNAME = 'projet1ipssi';
- $DBUSER = 'admin';
- $DBPASSWORD = 123;
-
-?>
--- php/connexion-db.php
+++
@@ -1,6 +0,0 @@
-<?php
-    
-    require('config-db.php');
-    $PDO=new PDO("$DBDRIVER:host=$DBHOST;dbname=$DBNAME", $DBUSER, $DBPASSWORD);
-
-?> 
